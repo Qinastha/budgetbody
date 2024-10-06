965ac0dc957ee9db9d663fd0f8e9dff62825c169
Anton <nanderson@banjohealth.com>
2024-10-06 22:12:40 +0200

    update dashboard and expenses

--- src/Components/PeriodItem/PeriodItem.tsx
+++ src/Components/PeriodItem/PeriodItem.tsx
@@ -1,30 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./PeriodItem.scss";
 import { TimeResolution } from "../../core";
 
 interface PeriodItemProps {
-  activePeriod: TimeResolution;
-  handlePeriodChange: (period: TimeResolution) => void;
+    activePeriod: TimeResolution;
+    handlePeriodChange: (period: TimeResolution) => void;
 }
 
 export const PeriodItem: React.FC<PeriodItemProps> = ({
-  activePeriod,
-  handlePeriodChange,
-}) => {
-  const periods: TimeResolution[] = ["1m", "3m", "6m", "1y"];
+                                                          activePeriod,
+                                                          handlePeriodChange,
+                                                      }) => {
+    const periods: TimeResolution[] = ["1m", "3m", "6m", "1y"];
 
-  return (
-    <div className="periodItemContainer">
-      <ul>
-        {periods.map((period: TimeResolution) => (
-          <li
-            key={period}
-            className={activePeriod === period ? "active" : ""}
-            onClick={() => handlePeriodChange(period)}>
-            {period}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="periodItemContainer">
+            <ul>
+                {periods.map((period: TimeResolution) => (
+                    <li
+                        key={period}
+                        className={activePeriod === period ? "active" : ""}
+                        onClick={() => handlePeriodChange(period)}>
+                        {period}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
--- src/core/index.ts
+++ src/core/index.ts
@@ -3,3 +3,4 @@ export * from "./components";
 export * from "./interfaces";
 export * from "./utils";
 export * from "./enums";
+export * from './constants'
