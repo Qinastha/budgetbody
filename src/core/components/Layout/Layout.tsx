d03008dee3c5d0a4d1213801dc0da89bbd84dfdf
Anton <nanderson@banjohealth.com>
2024-10-16 00:17:05 +0200

    add month updates

--- src/core/components/Layout/Layout.tsx
+++ src/core/components/Layout/Layout.tsx
@@ -1,10 +1,35 @@
-import React, { useRef, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import "./Layout.scss";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../Navbar/Navbar";
+import {useAppDispatch, useAppSelector} from "../../../hooks";
+import {getLastIncome, getUser, getUserCurrency, updateMonthFinances} from "../../../store/userSlice";
+import {IUser} from "../../interfaces";
 
 export const Layout: React.FC = () => {
   const [isNavbarHidden, setIsNavbarHidden] = useState<boolean>(false);
+  const lastIncome = useAppSelector(getLastIncome)
+  const user:IUser = useAppSelector(getUser);
+  const userCurrency = useAppSelector(getUserCurrency)
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if(lastIncome && lastIncome.timestamp.getMonth() !== new Date(Date.now()).getMonth()
+        && lastIncome.timestamp.getFullYear() !== new Date(Date.now()).getFullYear()){
+      const {monthIncome,monthHealthcare,monthTax,monthCredit,monthHousing,monthOther} = user.applicationSettings
+      console.log('dasfasd')
+      console.log(lastIncome)
+      console.error(monthIncome[userCurrency.code])
+        dispatch(updateMonthFinances({
+          monthIncome: monthIncome[userCurrency.code],
+          monthCredit: monthCredit[userCurrency.code],
+          monthHealthcare: monthHealthcare[userCurrency.code],
+          monthHousing: monthHousing[userCurrency.code],
+          monthOther: monthOther[userCurrency.code],
+          monthTax: monthTax[userCurrency.code]
+        }))
+    }
+  },[])
 
   const toggleNavbar = (e: React.TouchEvent) => {
     e.stopPropagation();
--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -162,6 +162,31 @@ export const updateApplicationSettingsData = createAsyncThunk(
   },
 );
 
+export const updateMonthFinances = createAsyncThunk(
+    "userData/updateMonthFinances",
+    async (data: any) => {
+        try {
+            const response = await axios.put(
+                `${process.env.REACT_APP_BASE_URL}/time-series/month-update`,
+                data,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    },
+                },
+            );
+            if (response?.data?.value) {
+                return response.data.value;
+            } else {
+                console.error("Failed to update application settings");
+            }
+        } catch (err) {
+            throw err;
+        }
+    },
+);
+
 export const deleteUserProfile = createAsyncThunk(
   "userData/deleteUserProfile",
   async () => {
@@ -310,6 +335,12 @@ export const user = createSlice({
     getExpensesActivePeriod: state =>
       state.analytics.expensesAnalytics.timeResolution,
     getUserTheme: state => state.applicationSettings.theme,
+      getLastIncome: state => {
+        console.log('check in state')
+          console.log(state)
+          console.log([...state.incomes].sort((a: ITimeSeries,b: ITimeSeries) => b.timestamp.getTime() - a.timestamp.getTime()))
+          return state.incomes.length > 0 ? [...state.incomes].sort((a: ITimeSeries,b: ITimeSeries) => b.timestamp.getTime() - a.timestamp.getTime())[0] : null
+      }
   },
   extraReducers: builder => {
     builder.addCase(
@@ -348,6 +379,15 @@ export const user = createSlice({
         };
       },
     );
+      builder.addCase(
+          updateMonthFinances.fulfilled,
+          (state, action: PayloadAction<IUser>) => {
+              return {
+                  ...state,
+                  ...action.payload,
+              };
+          },
+      );
     builder.addCase(deleteUserProfile.fulfilled, state => {
       return initialState;
     });
@@ -401,6 +441,7 @@ export const {
   getDashboardActivePeriod,
   getExpensesActivePeriod,
   getUserTheme,
+    getLastIncome
 } = user.selectors;
 
 export default user.reducer;
